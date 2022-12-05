248d793e663ed060eb7ecbf3f26614bfa51287bf
Kaizzz-00 <15359435358zck@gmail.com>
2022-12-05 16:17:07 -0500

    MaterialPurchasing_withCRUD

---
+++ src/main/java/com/fp_1/model/MaterialPurchasing/StorageMaterial.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fp_1.model.MaterialPurchasing;
+
+/**
+ *
+ * @author kai
+ */
+public class StorageMaterial {
+    String materialName;
+    Double weight;
+    Double RealUnitPrice;
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getRealUnitPrice() {
+        return RealUnitPrice;
+    }
+
+    public void setRealUnitPrice(Double RealUnitPrice) {
+        this.RealUnitPrice = RealUnitPrice;
+    }
+    
+    
+}
