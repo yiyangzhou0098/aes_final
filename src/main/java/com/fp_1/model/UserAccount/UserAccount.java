9bafc4476476e1a77ecbc8a526b262bbc9475dd0
Kaizzz-00 <15359435358zck@gmail.com>
2022-12-09 00:18:57 -0500

    Sysadmin_useraccountEdit

---
+++ src/main/java/com/fp_1/model/UserAccount/UserAccount.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fp_1.model.UserAccount;
+
+/**
+ *
+ * @author kai
+ */
+public class UserAccount {
+    String userName;
+    String userPassword;
+    String userIdentity;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserIdentity() {
+        return userIdentity;
+    }
+
+    public void setUserIdentity(String userIdentity) {
+        this.userIdentity = userIdentity;
+    }
+    
+    
+}
